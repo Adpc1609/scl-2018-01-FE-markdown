568192dd764e9b229270530e1aa37dae9ce13fb4
Adriana <adpc1609@gmail.com>
2018-08-29 00:10:38 -0300

    limpiando archivo

--- index.js
+++ index.js
@@ -9,81 +9,74 @@ const Marked = require('marked');
 
 let urlFile = argv._[0];
 console.log(argv, argv.validate);
-    /** TODO
-    * Se comprueba que la ruta no sea relativa, en caso de serlo
-    * se setea una ruta, basa en la ubicacion del proyecto 
-    **/
-    if(!path.isAbsolute(urlFile)) {
-        urlFile = process.cwd()+'/'+urlFile;
-        console.log('relativa',urlFile);
+/** TODO
+* Se comprueba que la ruta no sea relativa, en caso de serlo
+* se setea una ruta, basada en la ubicacion del proyecto 
+**/
+if (!path.isAbsolute(urlFile)) {
+  urlFile = process.cwd() + '/' + urlFile;
+  console.log('relativa', urlFile);
+}
+
+
+/** TODO
+* Se valida la extension del archivo, en caso de no tener se retorna y no se sigue el programa 
+**/
+if (path.extname(urlFile) !== '.md')
+  return;
+
+mdLinks(urlFile, argv);
+
+function mdLinks(urlFile, argv) {
+
+  fs.readFile(urlFile, 'utf-8', (err, data) => {
+    if (err) {
+      console.log('error: ', err);
+    } else {
+      console.log(data);
+      let response = markdownLinkExtractor(data);
+      console.log(response);
     }
+  });
 
 
-    /** TODO
-    * Se valida la extension del archivo, en caso de no tener se retorna y no se sigue el programa 
-    **/
-   if(path.extname(urlFile)!=='.md')
-        return;
+  if (argv.validate || argv.stats) {
+    console.log('argumentos validate');
 
-      mdLinks(urlFile, argv);
-        
-    console.log('llegue al final');
-    
+  } else {
+    console.log('sin argumentos');
+  }
 
-    // Función necesaria para extraer los links usando marked
-    // (tomada desde biblioteca del mismo nombre y modificada para el ejercicio)
-    // Recibe texto en markdown y retorna sus links en un arreglo
-    function mdLinks(urlFile, argv) {
+};
 
-      fs.readFile(urlFile, 'utf-8', (err, data) => {
-        if(err) {
-          console.log('error: ', err);
-        } else {
-          console.log(data);
-          var response = markdownLinkExtractor(data);
-          console.log(response);
-        }
-      });
+//funcion que extrae los links del archivo.md
+function markdownLinkExtractor(markdown) {
+  const links = [];
 
+  const renderer = new Marked.Renderer();
 
-      if(argv.validate || argv.stats){
-        console.log('argumentos validate');
-        
-      }else{
-        console.log('sin argumentos');
-      }
+  const linkWithImageSizeSupport = /^!?\[((?:\[[^\[\]]*\]|\\[\[\]]?|`[^`]*`|[^\[\]\\])*?)\]\(\s*(<(?:\\[<>]?|[^\s<>\\])*>|(?:\\[()]?|\([^\s\x00-\x1f()\\]*\)|[^\s\x00-\x1f()\\])*?(?:\s+=(?:[\w%]+)?x(?:[\w%]+)?)?)(?:\s+("(?:\\"?|[^"\\])*"|'(?:\\'?|[^'\\])*'|\((?:\\\)?|[^)\\])*\)))?\s*\)/;
 
-    };
+  Marked.InlineLexer.rules.normal.link = linkWithImageSizeSupport;
+  Marked.InlineLexer.rules.gfm.link = linkWithImageSizeSupport;
+  Marked.InlineLexer.rules.breaks.link = linkWithImageSizeSupport;
 
-    function markdownLinkExtractor(markdown) {
-      const links = [];
-    
-      const renderer = new Marked.Renderer();
-    
-      // Taken from https://github.com/markedjs/marked/issues/1279
-      const linkWithImageSizeSupport = /^!?\[((?:\[[^\[\]]*\]|\\[\[\]]?|`[^`]*`|[^\[\]\\])*?)\]\(\s*(<(?:\\[<>]?|[^\s<>\\])*>|(?:\\[()]?|\([^\s\x00-\x1f()\\]*\)|[^\s\x00-\x1f()\\])*?(?:\s+=(?:[\w%]+)?x(?:[\w%]+)?)?)(?:\s+("(?:\\"?|[^"\\])*"|'(?:\\'?|[^'\\])*'|\((?:\\\)?|[^)\\])*\)))?\s*\)/;
-    
-      Marked.InlineLexer.rules.normal.link = linkWithImageSizeSupport;
-      Marked.InlineLexer.rules.gfm.link = linkWithImageSizeSupport;
-      Marked.InlineLexer.rules.breaks.link = linkWithImageSizeSupport;
-    
-      renderer.link = function(href, title, text) {
-        links.push({
-          href: href,
-          text: text,
-          title: title,
-        });
-      };
-      renderer.image = function(href, title, text) {
-          // Remove image size at the end, e.g. ' =20%x50'
-          href = href.replace(/ =\d*%?x\d*%?$/, '');
-          links.push({
-            href: href,
-            text: text,
-            title: title,
-          });
-      };
-      Marked(markdown, {renderer: renderer});
-    
-      return links;
-    };
+  renderer.link = function (href, title, text) {
+    links.push({
+      href: href,
+      text: text,
+      title: title,
+    });
+  };
+  renderer.image = function (href, title, text) {
+    href = href.replace(/ =\d*%?x\d*%?$/, '');
+    links.push({
+      href: href,
+      text: text,
+      title: title,
+    });
+  };
+  Marked(markdown, { renderer: renderer });
+
+  return links;
+};
